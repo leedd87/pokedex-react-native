4e5def024fbf7d0975682b1d90b625f16bb1f07b
leedd87 <leedav87@gmail.com>
2023-01-17 10:33:15 -0300

    get unique pokemons

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,18 +1,32 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { pokemonApi } from '../api/pokemonApi';
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
 
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([])
     const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
 
     const loadPokemons = async () => {
-        const resp = await pokemonApi.get(nextPageUrl.current)
-        console.log(resp.data)
+        const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current)
+        nextPageUrl.current = resp.data.next
+        mapPokemonList(resp.data.results)
+    }
+
+    const mapPokemonList = (pokemonList: Result[]) => {
+
+        pokemonList.forEach(poke => console.log(poke.name))
+
     }
 
     useEffect(() => {
         loadPokemons()
     }, [])
 
+
+
+    return {
+        simplePokemonList
+    }
 
 }
