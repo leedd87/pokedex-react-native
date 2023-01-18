f57724d6d5da15b1b7c7be2c9a1480be424a2a73
leedd87 <leedav87@gmail.com>
2023-01-18 11:41:35 -0300

    add pokemonCard to pokedex

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { ActivityIndicator, Button, FlatList, Image, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { createIconSetFromFontello } from 'react-native-vector-icons'
+import { FadeInImage } from '../components/FadeInImage'
+import { PokemonCard } from '../components/PokemonCard'
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated'
 import { styles } from '../theme/appTheme'
 
@@ -31,28 +33,35 @@ export const HomeScreen = () => {
                 source={require('../assets/pokebola.png')}
                 style={styles.pokebolaBG}
             />
-            <FlatList
-                data={simplePokemonList}
-                keyExtractor={(pokemon) => pokemon.id}
-                renderItem={({ item }) => (
-                    <Image
-                        source={{ uri: item.picture }}
-                        style={{
-                            width: 100,
-                            height: 100,
-                        }}
-                    />
-                )}
-                showsVerticalScrollIndicator={false}
-                onEndReached={loadPokemons}
-                onEndReachedThreshold={0.4}
-                ListFooterComponent={(
-                    <ActivityIndicator
-                        style={{ height: 100 }}
-                        size={20}
-                        color='red'
-                    />)}
-            />
+            <View style={{ alignItems: 'center' }}>
+                <FlatList
+                    data={simplePokemonList}
+                    keyExtractor={(pokemon) => pokemon.id}
+                    renderItem={({ item }) => (
+                        <PokemonCard pokemon={item} />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                    ListHeaderComponent={(
+                        <Text style={{
+                            ...styles.title,
+                            ...styles.globalMargin,
+                            top: top + 20,
+                            marginBottom: top + 20,
+                            paddingBottom: 10,
+                        }}>Pokedex</Text>
+
+                    )}
+                    onEndReached={loadPokemons}
+                    onEndReachedThreshold={0.4}
+                    ListFooterComponent={(
+                        <ActivityIndicator
+                            style={{ height: 100 }}
+                            size={20}
+                            color='red'
+                        />)}
+                />
+            </View>
         </>
     )
 }
